9f37963ff8ea09881c3958f5df9bd8e120b4e1e0
Jessica Geramanis-Bowden <jessica.mbowden@gmail.com>
2023-01-19 17:01:48 -0500

    testing

--- test_generic.py
+++ test_generic.py
@@ -1,11 +1,12 @@
 
 # a comment
+# another comment
 
 def test_generic():
     assert 3 == 3
 
-def test_failing():
-    assert 1 == 2
+# def test_failing():
+#     assert 1 == 2
 
 def test_str():
     assert "a" == "a"
